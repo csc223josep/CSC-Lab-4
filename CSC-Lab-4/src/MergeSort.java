b27af004c83ad84c64c73b2bdb3135c184fe091a
Isaiah <ia2099@email.vccs.edu>
2024-02-15 12:15:32 -0500

    More edits.

--- CSC-Lab-4/src/MergeSort.java
+++ CSC-Lab-4/src/MergeSort.java
@@ -1,19 +1,27 @@
+/**
+ * Merge Sort
+ * 
+ * @author Isaiah Ayres
+ * @author Dan Obrien
+ * @author Joseph Kabesha
+ */
+
 public class MergeSort {
     // Merges two subarrays of arr[]
-    public static void merge(int arr[], int l, int m, int r) {
+    public static void merge(int arr[], int left, int mid, int right) {
         // TODO- Finish coding to array generation & print routine
     }
     public static void main(String[] args) {
-        int arr[] = {}; // Create an array of 10 values
+        int arr[] = {13, 64, 23, 49, 32, 55, 70, 5, 94, 85}; // Create an array of 10 values
         System.out.println("Given Array");
-        for (int i=0; i<arr.length; ++i)
+        for (int i = 0; i < arr.length; ++i)
             System.out.print(arr[i] + " ");
         System.out.println();
 
         MergeSort.sort(arr, 0, arr.length-1);
 
         System.out.println("\nSorted array");
-        for (int i=0; i<arr.length; ++i)
+        for (int i = 0; i < arr.length; ++i)
             System.out.print(arr[i] + " ");
         System.out.println();
     }
--- CSC-Lab-4/src/QuickSort.java
+++ CSC-Lab-4/src/QuickSort.java
@@ -1,11 +1,17 @@
 import java.util.Random;
 
+/**
+ * Quick Sort
+ * 
+ * @author Isaiah Ayres
+ * @author Dan Obrien
+ * @author Joseph Kabesha
+ */
+
 public class QuickSort {
     public static void quickSort(int[] arr, int begin, int end) {
-
-// TODO- Finish coding from here print routine
-
-
+        // TODO- Finish coding from here print routine
+        
         System.out.println("Sorted array:");
         for (int value : arr) {
             System.out.print(value + " ");
